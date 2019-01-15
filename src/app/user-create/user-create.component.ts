4fcbd07c39364696887bd56608ef2f182b037f54
Mary PASCAL <mary.emilie@outlook.fr>
2019-01-15 18:35:01 +0100

    ajout sha512 mot de passe

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { sha512 } from 'js-sha512';
 
 @Component({
   selector: 'app-user-create',
@@ -18,6 +19,7 @@ export class UserCreateComponent implements OnInit {
   ngOnInit() {}
 
   addUser() {
+    this.user.password = sha512(this.user.password);
     this.http.post('http://localhost:8080/rest-in-pitch/rest/user/register', this.user).subscribe(res => {
         // login :
         this.http.post('http://localhost:8080/rest-in-pitch/rest/user/login', this.user,  { responseType: 'text'})
@@ -35,6 +37,7 @@ export class UserCreateComponent implements OnInit {
   }
 
   login() {
+    this.log.password = sha512(this.log.password);
     this.http.post('http://localhost:8080/rest-in-pitch/rest/user/login', this.log,  { responseType: 'text'})
       .subscribe(res => {
         localStorage.setItem('currentUser', res);
